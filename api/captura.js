ccfb381cf81c035e78694378a9e51d66b0e1eaef
Cauazitodev16 <rubislinscaua@gmail.com>
2026-05-05 16:44:53 -0300

    build: estudando design responsivo

--- api/captura.js
+++ api/captura.js
@@ -1,47 +1,52 @@
-const video = document.getElementById("cameraFeed")
-const canvas = document.getElementById("capturedImage")
+const video = document.getElementById("cameraFeed");
+const canvas = document.getElementById("capturedImage");
 const botao = document.getElementById("captureButton");
 const contexto = canvas.getContext("2d");
 
-// Solicitar permissão para acessar a câmera 
-navigator.mediaDevices.getUserMedia({ video: true })
+//solicicitar acesso à câmera
+navigator.mediaDevices.getUserMedia({ video: true})
     .then(stream => {
         video.srcObject = stream;
         video.play();
     })
-    .catch(erro => {
-        console.error("Erro ao acessar a câmera: ", erro);
+    .catch( erro => {
+        console.error("erro ao acessar a câmera: ", erro);
     });
 
-//atribuir uma função ao botão de captura
+//atribuir uma função para o botão de captura
+botao.addEventListener("click", () =>{
+    //desenhar o quadro atual do vídeo no área do canvas
+    contexto.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-botao.addEventListener("click", () => {
-    // Desenhar o quadro atual do vídeo na area do canvas
-    contexto.drawImage(video, 0, 0, canvas.width = 900, canvas.height = 150);
-
-    //obter a imagem capturada como uma URL de dados
-    const imagemCapturada = canvas.toDataURL("image/png");
-    //enviar a imagem para o servidor
+    //obter a imagem como um URL de dados
+    const imageDataURL = canvas.toDataURL("image/png");
+    //enviar a imagem para um servidor
     enviarImagemParaServidor(imageDataURL);
-});
+})
 
-//função para enviar a imagem para o servidor
+//função para enviar imagem para um servidor
 function enviarImagemParaServidor(imageDataURL){
+    //simular o envio salvando o dado no próprio computador
     console.log("Enviando imagem para o servidor...");
-    fetch("/", {
+
+    const base64String = imageDataURL.split(",")[1]; 
+
+    
+    fetch("http://dop3080-1247456:8000/images", {
         method: "POST",
-        body: JSON.stringify({ image: imageDataURL}),
+        body: JSON.stringify({image: base64String, mime_type: "image/png"}),
         headers: {
             "Content-Type": "application/json"
         }
-})
-    .then( resposta => resposta.json())
-    .then( dados => {
-        console.log("Resposta do servidor: ", dados);
     })
-    .catch( erro => {
-        console.error("Erro ao enviar a imagem: ", erro);
-    });
+        .then( resposta => resposta.json())
+        .then( dados => {
+            console.log("Resposta do servidor: ", dados);
+        })
+        .catch( erro => {
+            console.error("Erro ao enviar a imagem: ", erro);
+        })
+
 
 
 }
